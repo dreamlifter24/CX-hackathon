793aebfebf89fd0dfc3d7553d2bd8a296827d7f1
ivankcl <ivankwong4c@gmail.com>
2021-11-13 16:39:34 +0800

    Updated NotificationsFragment

--- app/src/main/java/com/example/bottomnavigation/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/bottomnavigation/ui/notifications/NotificationsFragment.java
@@ -12,6 +12,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.bottomnavigation.R;
 import com.example.bottomnavigation.databinding.FragmentNotificationsBinding;
 
@@ -20,6 +30,10 @@ public class NotificationsFragment extends Fragment {
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
 
+//    EditText username;
+//    EditText password;
+//    Button logIn;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
@@ -27,8 +41,29 @@ public class NotificationsFragment extends Fragment {
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
+
+//        username = (EditText)findViewById(R.id.editText1);
+//        password = (EditText)findViewById(R.id.editText2);
+//
+//        logIn = (Button)findViewById(R.id.logIn);
+
+        binding.logIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(binding.editText1.getText().toString().equals("admin") && binding.editText2.getText().toString().equals("admin")){
+                    Toast.makeText(getActivity(), "Logging in..", Toast.LENGTH_SHORT).show();
+                    //correcct password
+                }else{
+                    Toast.makeText(getActivity(), "Failed", Toast.LENGTH_SHORT).show();
+                    //wrong password
+                }
+            }
+        });
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
